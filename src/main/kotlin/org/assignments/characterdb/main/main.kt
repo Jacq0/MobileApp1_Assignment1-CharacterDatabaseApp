21fc3ec8c548b729d54758f0ecdc7e8a10f4825d
Jacq0 <59923922+Jacq0@users.noreply.github.com>
2022-09-24 23:20:33 +0100

    Added some baseline code

--- src/main/kotlin/org/assignments/characterdb/main/main.kt
+++ src/main/kotlin/org/assignments/characterdb/main/main.kt
@@ -1,8 +1,30 @@
-package org.assignments.characterdb
+package org.assignments.characterdb.main
+
+import org.assignments.characterdb.models.CharacterModel
+
+var character = CharacterModel() //base character model with no data
+var characters =  ArrayList<CharacterModel>() //array list of all characters
 
 fun main()
 {
-    println("initial repo main file.")
+    println("Welcome to the Character Database App v1.0")
+
+    var input: Int = -1
+
+    do
+    {
+        input = menu()
+        when(input)
+        {
+            1 -> addCharacter()
+            2 -> updateCharacter()
+            3 -> getCharacterList()
+            4 -> deleteCharacter()
+            0 -> println("Bye Bye o7")
+            else -> println("Invalid Option Selected")
+        }
+    }
+    while(input != 0)
 }
 
 fun menu() : Int
@@ -11,14 +33,69 @@ fun menu() : Int
     var option: Int
     var input: String? = null
 
-    println("Welcome to the Character Database App v1.0")
     println("1 - Add a Character")
     println("2 - Update existing Character")
     println("3 - View Characters")
     println("4 - Delete a Characer")
+    println("0 - Exit Application")
 
     input = readLine()!!
     option = input.toInt()
 
     return option
+}
+
+fun addCharacter()
+{
+    println("------------------")
+    println("Add Character")
+    println("------------------")
+
+    println("Enter Character Name: ")
+    character.name = readLine()!!
+
+    println("Enter Character Description: ")
+    character.description = readLine()!!
+
+    println("Enter Character Occupation: ")
+    character.occupations = readLine()!!
+
+    println("Enter Characters First Appearance: ")
+    character.originalAppearance = readLine()!!
+
+    println("Enter Characters First Appearance Year: ")
+    character.originalAppearanceYear = readLine()!!.toInt()
+
+    //check if we can add the character with this data
+    if(allMandatoryFieldsFilled(character))
+    {
+        characters.add(character)
+    }
+    else
+    {
+        //couldn't add character, throw error to user.
+    }
+}
+
+fun allMandatoryFieldsFilled(character: CharacterModel): Boolean
+{
+    return true
+}
+
+fun updateCharacter()
+{
+
+}
+
+fun getCharacterList()
+{
+    for (c in characters)
+    {
+        println(c)
+    }
+}
+
+fun deleteCharacter()
+{
+
 }
--- src/main/kotlin/org/assignments/characterdb/models/CharacterModel.kt
+++ src/main/kotlin/org/assignments/characterdb/models/CharacterModel.kt
@@ -1,4 +1,12 @@
 package org.assignments.characterdb.models
 
-class CharacterModel {
-}
+//note: occupations can be a list of strings that a user could add to and remove from.
+//the rating can be an accumulation of user ratings for the character/character page with a bit of maths, could also be a list
+//could have a character image/gallery here too, but we can save that for later.
+data class CharacterModel(var id: Long = 0,
+                          var name: String = "",
+                          var description: String = "",
+                          var occupations: String = "",
+                          var originalAppearance: String = "",
+                          var originalAppearanceYear: Int? = null,
+                          var rating: Int? = null) {}
